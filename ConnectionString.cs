8f7d8385e26d5ea4cb2d1fed6eaadccbec053760
VenkataSivaReddyPadala <siva@logics.com>
2024-03-19 21:29:59 +0530

    Implemented attendance system

---
+++ ConnectionString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+
+namespace MultiFaceRec
+{
+   public class ConnectionString
+    {
+       public string DBConn = @"Data Source=LOGICS;Initial Catalog=Attendance;Integrated Security=True;";
+    }
+}
